b646d4182277a47860ea75d32f863ad34fe990e8
tarzansos <tuminfei1981@gmail.com>
2017-08-04 11:55:13 +0800

    RSA

--- lib/suning_pay/rsa.rb
+++ lib/suning_pay/rsa.rb
@@ -1,21 +1,21 @@
 #encoding: utf-8
 module SuningPay
   module RSA
+    #SuningPay 提供的KEY长度为2048
     MAX_ENCRYPT_LENGTH = 245
     MAX_DECRYPT_LENGTH = 256
+    #MAX_ENCRYPT_LENGTH = 117
+    #MAX_DECRYPT_LENGTH = 128
 
     #分段加密
     def self.encrypt_msg(key, message)
-      code = Base64.encode64(message)
-      puts code
-      bytes_array = code.unpack("C*")
+      bytes_array = message.unpack("C*")
       input_length = bytes_array.length
 
       encryt_str, offset, i = "", 0, 0
       begin
         encryt_bytes = bytes_array[offset, MAX_ENCRYPT_LENGTH]
-        #加密
-        encryt_str << key.public_encrypt(encryt_bytes.pack("C*"))
+        encryt_str << key.public_encrypt(encryt_bytes.pack("C*")) #加密
         offset = (i += 1) * MAX_ENCRYPT_LENGTH
       end while input_length - offset > 0
 
@@ -30,8 +30,7 @@ module SuningPay
       decryt_str, offset, i = "", 0, 0
       begin
         decryt_bytes = bytes_array[offset, MAX_DECRYPT_LENGTH]
-        #解密
-        decryt_str << key.private_decrypt(decryt_bytes)
+        decryt_str << key.private_decrypt(decryt_bytes) #解密
         offset = (i += 1) * MAX_DECRYPT_LENGTH
       end while input_length - offset > 0
 
