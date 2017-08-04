9c9d13ad09631155d73cf5af6da15be30743fc0c
tarzansos <tuminfei1981@gmail.com>
2017-08-04 13:57:55 +0800

    doc

--- suning_pay.rb
+++ suning_pay.rb
@@ -1,11 +1,16 @@
-#TEST
+##################TEST##################
+#接口地址
 SuningPay.api_base_url = 'https://ebanksandbox.suning.com/epps-ebpg/api/contract/'
+#客户号
 SuningPay.merchant_no = '70057278'
+#Suning证书存放路径
 SuningPay.api_suning_cert = ''
+#客户端RSA 公钥,1024, pem文件路径
 SuningPay.api_client_public_key = ''
+#客户端RSA 私钥,1024, pem文件路径
 SuningPay.api_client_private_key = ''
 
-#PROD
+##################PROD##################
 #SuningPay.api_base_url = 'https://ebankpay.suning.com/epps-ebpg/api/contract/'
 #SuningPay.merchant_no = ''
 #SuningPay.api_suning_cert = ''
